97d12d25d690b6ac9b895bb6ff78200556aefa77
PrathmeshKapde <prathmkapde17@gmail.com>
2025-08-11 20:11:57 +0530

    Page1

--- CAPM_Returns.py
+++ CAPM_Returns.py
@@ -11,15 +11,17 @@ st.set_page_config(page_title = "CAPM",
     page_icon = "chart_with_upwards_trend",
     layout = 'wide') 
 
-st.title("Capital Asset Pricing Model")
+st.title("Capital Asset Pricing Model with Stock Forecasting 💰")
+st.caption("The Capital Asset Pricing Model (CAPM) describes the relationship between systematic risk, or the general perils of investing, and expected return for assets, particularly stocks.It is a finance model that establishes a linear relationship between the required return on an investment and risk.")
+
 
 # Getting input from user
 
 col1, col2 = st.columns([1,1])
 with col1:
-    stock_list = st.multiselect("Choose 4 stocks", ('TSLA', 'AAPL', 'NFLX', 'MSFT', 'MGM', 'AMZN', 'NVDA', 'GOOGL'), ['TSLA', 'AAPL', 'AMZN', 'GOOGL'])
+    stock_list = st.multiselect("Choose stocks by ticker",('AAPL', 'MSFT', 'AMZN', 'NVDA', 'GOOGL', 'TSLA', 'META', 'GOOG', 'BRK', 'UNH', 'XOM', 'JNJ', 'JPM', 'V', 'LLY', 'AVGO', 'PG', 'MA', 'HD', 'MRK', 'CVX', 'PEP', 'COST', 'ABBV', 'KO', 'ADBE', 'WMT', 'MCD', 'CSCO', 'PFE', 'CRM', 'TMO', 'BAC', 'NFLX', 'ACN', 'A','DE', 'GS', 'ELV', 'LMT', 'AXP', 'BLK', 'SYK', 'BKNG', 'MDLZ', 'ADI', 'TJX', 'GILD', 'MMC', 'ADP', 'VRTX', 'AMT', 'C', 'CVS', 'LRCX', 'SCHW', 'CI', 'MO', 'ZTS', 'TMUS', 'ETN', 'CB', 'FI'),('AAPL','TSLA','AMZN','GOOGL'))
 with col2:
-    year = st.number_input("Number of years",1,10)
+    year = st.number_input("Years of investment",1,10)
 
 # Downloading data for SP500
 try:
@@ -41,26 +43,26 @@ try:
     stocks_df['Date'] = stocks_df['Date'].apply(lambda x:str(x)[:10])
     stocks_df['Date'] = pd.to_datetime(stocks_df['Date'])
     stocks_df = pd.merge(stocks_df, SP500, on = 'Date', how = 'inner')
-    print(stocks_df) 
 
     col1, col2 = st.columns([1,1])
     with col1:
-        st.markdown("### Dataframe head")
+        st.markdown("### Stock Prices (Head)")
         st.dataframe(stocks_df.head(), use_container_width = True)
     with col2:
-        st.markdown("### Dataframe tail")
+        st.markdown("### Stock Prices (Tail)")
         st.dataframe(stocks_df.tail(), use_container_width = True)
 
     col1, col2 = st.columns([1,1])
     with col1:
         st.markdown("### Price of all the Stocks")
+        st.caption("initial stock prices")
         st.plotly_chart(capm_functions.interactive_plot(stocks_df))
     with col2:
         st.markdown("### Price of all the Stocks (After Normalizing)")
+        st.caption("prices being normalized over initial stock prices")
         st.plotly_chart(capm_functions.interactive_plot(capm_functions.normalize(stocks_df)))
 
     stocks_daily_return = capm_functions.daily_return(stocks_df)
-    print(stocks_daily_return.head())
 
     beta  = {}
     alpha = {}
@@ -68,17 +70,16 @@ try:
     for i in stocks_daily_return.columns:
         if i !='Date' and i != 'sp500':
             b, a = capm_functions.calculate_beta(stocks_daily_return, i)
-
             beta[i] = b
             alpha[i] = a
-    print(beta, alpha)
 
-    beta_df = pd.DataFrame(columns = ['Stocks', 'Beta Value'])
+    beta_df = pd.DataFrame(columns = ['Stock', 'Beta Value'])
     beta_df['Stock'] = beta.keys()
     beta_df['Beta Value'] = [str(round(i,2)) for i in beta.values()]
 
     with col1:
-        st.markdown('### Calculated Beta Value')
+        st.markdown('### Calculated Risk (β)')
+        st.caption("risk of market is considered as 1")
         st.dataframe(beta_df, use_container_width = True)
 
     rf = 0
@@ -86,15 +87,16 @@ try:
 
     return_df = pd.DataFrame()
     return_value = []
-    for stock, value in beta.items():
-        return_value.append(str(round(rf+(value*(rf-rm)),2)))
+    for stocks, value in beta.items():
+        return_value.append(str(round(rf+(value*(rm-rf)),2)))
     return_df['Stock'] = stock_list
 
     return_df['Return Value'] = return_value
 
     with col2:
         st.markdown('### Calculated Return using CAPM')
+        st.caption("the risk-free rate + the beta (or risk) of the investment * the expected return on the market - the risk free rate")
         st.dataframe(return_df, use_container_width = True) 
 
 except:
-    st.write("Please select valid inputs")
+    st.write("Please select valid input...")
--- capm_functions.py
+++ capm_functions.py
@@ -6,7 +6,7 @@ def interactive_plot(df):
     fig = px.line()
     for i in df.columns[1:]:
         fig.add_scatter(x = df['Date'], y = df[i], name = i)
-    fig.update_layout(width = 450, margin = dict(l=20, r=20, t=20, b=20), legend = dict(orientation = 'h', yanchor = 'bottom',
+    fig.update_layout(width = 450, margin = dict(l=20, r=20, t=50, b=20), legend = dict(orientation = 'h', yanchor = 'bottom',
         y = 1.02, xanchor = 'right', x = 1,))
     return fig
 
@@ -24,7 +24,7 @@ def daily_return(df):
     df_daily_return = df.copy()
     for i in df.columns[1:]:
         for j in range(1,len(df)):
-            df_daily_return[i][j] = (df[i][j]-df[i][j-1])/df[i][j-1]*100
+            df_daily_return[i][j] = ((df[i][j]-df[i][j-1])/df[i][j-1])*100
         df_daily_return[i][0] = 0
     return df_daily_return
 
